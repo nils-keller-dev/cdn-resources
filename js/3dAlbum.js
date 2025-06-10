54a0dcc5e12278b3c276ba99bd489ab0652f1e21
tsomic <keller.nils99@gmail.com>
2025-06-10 16:41:28 +0200

    ♻️ use @latest version of images

--- js/3dAlbum.js
+++ js/3dAlbum.js
@@ -1,6 +1,6 @@
 export const render3DAlbum = (projectName) => {
   const BASE_URL =
-    "https://cdn.jsdelivr.net/gh/nils-keller-dev/cdn-resources/images/";
+    "https://cdn.jsdelivr.net/gh/nils-keller-dev/cdn-resources@latest/images/";
 
   document.querySelectorAll("div").forEach((el) => {
     el.style.backgroundImage = `url(${BASE_URL}${projectName}/${el.id}.webp)`;
