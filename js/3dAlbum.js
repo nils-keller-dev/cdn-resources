6943f1b71518ad7669892a18d4c47167d31a7a4e
tsomic <keller.nils99@gmail.com>
2025-06-12 21:06:13 +0200

    ✨ add shadows

--- js/3dAlbum.js
+++ js/3dAlbum.js
@@ -1,13 +1,37 @@
 export const render3DAlbum = (projectName) => {
-  const BASE_URL =
-    "https://cdn.jsdelivr.net/gh/nils-keller-dev/cdn-resources@latest/images/";
+	const BASE_URL =
+		"https://cdn.jsdelivr.net/gh/nils-keller-dev/cdn-resources@latest/images/";
 
-  document.querySelectorAll("div").forEach((el) => {
-    el.style.backgroundImage = `url(${BASE_URL}${projectName}/${el.id}.webp)`;
-    el.style.translate = `0 0 ${el.innerHTML}em`;
-  });
+	const generateImageUrl = (name) =>
+		`url(${BASE_URL}${projectName}/${name}.webp)`;
 
-  document.addEventListener("click", () =>
-    document.body.classList.toggle("disabled")
-  );
+	const shadowTemplate = document.createElement("div");
+	shadowTemplate.classList.add("shadow");
+
+	const images = [];
+
+	for (const el of [...document.querySelectorAll("div")].reverse()) {
+		const depth = Number(el.innerHTML);
+
+		el.style.setProperty("--image", generateImageUrl(el.id));
+		el.style.setProperty("--depth", `${depth}em`);
+
+		if (images.at(-1)?.depth > depth) {
+			images.forEach(({ name, depth: previousDepth }) => {
+				const shadowEl = shadowTemplate.cloneNode();
+				shadowEl.style.backgroundImage = generateImageUrl(name);
+				shadowEl.style.setProperty("--blur", `${(previousDepth - depth) / 10}em`);
+				el.appendChild(shadowEl);
+			});
+		}
+
+		images.push({
+			name: el.id,
+			depth
+		});
+	}
+
+	document.addEventListener("click", () =>
+		document.body.classList.toggle("disabled")
+	);
 };
