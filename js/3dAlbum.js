f1969eebd795c864d497b27debe9582836476fd9
tsomic <keller.nils99@gmail.com>
2025-06-22 23:09:44 +0200

    ✨ make shadows more adjustable

--- js/3dAlbum.js
+++ js/3dAlbum.js
@@ -5,34 +5,72 @@ export const render3DAlbum = (projectName) => {
 	const generateImageUrl = (name) =>
 		`url(${BASE_URL}${projectName}/${name}.webp)`;
 
+	const getShadow = (childIndex, parentIndex, parentImage) => {
+		if (!parentImage.classes.length) return parentIndex;
+
+		const indexDelta = childIndex - parentIndex;
+
+		let offset = 0;
+		let hasShadow = true;
+
+		for (const count of parentImage.classes) {
+			if (indexDelta <= offset + count) {
+				return hasShadow ? parentIndex : null;
+			}
+
+			offset += count;
+			hasShadow = !hasShadow;
+		}
+
+		return hasShadow ? parentIndex : null;
+	};
+
 	const shadowTemplate = document.createElement("div");
 	shadowTemplate.classList.add("shadow");
 
 	const images = [];
 
-	for (const el of [...document.querySelectorAll("div")].reverse()) {
-		const depth = Number(el.innerHTML);
+	for (const [currentIndex, el] of [...document.querySelectorAll("div")]
+		.reverse()
+		.entries()) {
+		const depth = Number(el.innerText);
 
 		el.style.setProperty("--image", generateImageUrl(el.id));
 		el.style.setProperty("--depth", `${depth}em`);
 
-		if (!el.classList.contains("r")) {
-			images
-				.filter((i) => i.depth > depth)
-				.forEach(({ name, depth: previousDepth }) => {
-					const shadowEl = shadowTemplate.cloneNode();
-					shadowEl.style.backgroundImage = generateImageUrl(name);
-					shadowEl.style.setProperty("--blur", `${(previousDepth - depth) / 10}em`);
-					el.appendChild(shadowEl);
-				});
-		}
+		const currentImage = {
+			el,
+			name: el.id,
+			classes: el.classList.value ? el.classList.value.split(" ").map(Number) : [],
+			depth
+		};
 
-		el.classList.contains("t") ||
-			images.push({
-				name: el.id,
-				depth
-			});
+		let shadows = [];
+		images.forEach((parentImage, parentIndex) => {
+			const shadow = getShadow(
+				currentIndex,
+				parentIndex,
+				parentImage
+			);
+			shadow !== null && shadows.push(shadow);
+		});
+
+		images.push({
+			...currentImage,
+			shadows
+		});
 	}
+
+	images.forEach(({ shadows, el, depth }) =>
+		shadows.forEach((shadowIndex) => {
+			const shadow = images[shadowIndex];
+
+			const shadowEl = shadowTemplate.cloneNode();
+			shadowEl.style.backgroundImage = generateImageUrl(shadow.name);
+			shadowEl.style.setProperty("--blur", `${(shadow.depth - depth) / 10}em`);
+			el.appendChild(shadowEl);
+		})
+	);
 
 	document.addEventListener("click", () =>
 		document.body.classList.toggle("disabled")
